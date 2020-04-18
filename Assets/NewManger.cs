1694301722c739f11e41b1bcd11f2a0bda510809
ChockBY <=894304434@qq.com>
2020-04-18 17:02:03 +0800

    实打实

---
+++ Assets/NewManger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewManger : MonoBehaviour {
+
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        text.text = "sdas获得一个";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
