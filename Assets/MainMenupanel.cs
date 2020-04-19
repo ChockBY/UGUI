9699aa954099ffe9185e1d308313d3462f598858
ChockBY <=894304434@qq.com>
2020-04-19 12:54:37 +0800

    修改

--- Assets/MainMenupanel.cs
+++ Assets/MainMenupanel.cs
@@ -21,11 +21,6 @@ public class MainMenupanel : BasePanel {
     {
         UIPanelType panelType = (UIPanelType)System.Enum.Parse(typeof(UIPanelType), panelTypestring);
         UIManager.Instance.PushPanel(panelType);
-<<<<<<< HEAD
-=======
-        print("我修改的第一个地方");
-        print("这是我最后一次修改得地方");
-        print("这是我修改得第二次");
->>>>>>> 39e17d6e54b1fee1e816f5249b3650a611502893
+
     }
 }
